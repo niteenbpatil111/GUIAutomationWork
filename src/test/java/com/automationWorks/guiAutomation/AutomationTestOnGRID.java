77747fcff7410e2cf9808bf00c22084b0fb6d29f
Nitin Patil <Nitin_Patil@epam.com>
2019-02-16 13:07:11 +0530

    update grid testcase

--- src/test/java/com/automationWorks/guiAutomation/AutomationTestOnGRID.java
+++ src/test/java/com/automationWorks/guiAutomation/AutomationTestOnGRID.java
@@ -18,33 +18,32 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
 
-public class AutomationTest2 {
+public class AutomationTestOnGRID {
 
 	public static WebDriver driver;
 
 	@BeforeTest
-	public void beforeMethod() throws MalformedURLException {
+	public void beforeTest() throws MalformedURLException {
 
 		DesiredCapabilities caps = new DesiredCapabilities();
 		caps = DesiredCapabilities.firefox();
 		caps.setPlatform(Platform.WINDOWS);
+
 		System.out.println("<<<<<<<<<<<<<<<<<<<<<<<<<" + System.getProperty("user.dir") + ">>>>>>>>>>>>>>>>>>");
-		// System.setProperty("webdriver.chrome.driver", "browsers\\chromedriver.exe");
-		// driver = new ChromeDriver();
+
 		try {
 			driver = new RemoteWebDriver(new URL("http://192.168.0.100:4444/wd/hub"), caps);
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
 		}
-		// System.setProperty("webdriver.gecko.driver", "browsers\\geckodriver.exe");
-		// driver = new FirefoxDriver();
+
 		driver.manage().window().fullscreen();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
 	}
 
 	@AfterTest
-	public void afterMethod() {
+	public void afterTest() {
 		driver.quit();
 	}
 
@@ -54,16 +53,15 @@ public class AutomationTest2 {
 		System.out.println(driver.getTitle() + " of test1 method");
 	}
 
-//	@Test
-//	public void test2() {
-//		driver.navigate().to("http://www.google.com");
-//		System.out.println(driver.getTitle() + " of test2 method");
-//	}
-//
-//	@Test
-//	public void test3() {
-//		driver.navigate().to("http://www.google.com");
-//		System.out.println(driver.getTitle() + " of test3 method");
-//	}
+	@Test
+	public void test2() {
+		driver.navigate().to("http://www.google.com");
+		System.out.println(driver.getTitle() + " of test2 method");
+	}
 
+	@Test
+	public void test3() {
+		driver.navigate().to("http://www.google.com");
+		System.out.println(driver.getTitle() + " of test3 method");
+	}
 }
